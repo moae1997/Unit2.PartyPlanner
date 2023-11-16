a240ad0f9c546b577e11e46f9d7c4739a27c83ec
Amna Abdallah <amnaabdallah@Amnas-MacBook-Air.local>
2023-11-16 18:29:24 -0500

    updating functions and added a form

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const eventsApiUrl = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-acc-et-web-pt-b/events";
 const eventList = document.getElementById("eventList");
+const dButton = document.createElement("button");
 
 const state = {
     events: []
 };
 
 async function render() {
-    getEvents();
+    await getEvents();
+    renderEvents();
+
 }
 
 async function getEvents() {
     let fetchApi = await fetch(eventsApiUrl);
     let resultEvents = await fetchApi.json();
-    let events = resultEvents.data;
-    events.map(event=> {
+    state["events"] = resultEvents.data;
+}
+
+async function renderEvents() {
+    state["events"].map(event=>{
         let eventName = document.createElement("li");
         eventName.innerHTML = event["name"];
         let eventdesc = document.createElement("li");
@@ -24,9 +30,22 @@ async function getEvents() {
         eventlocation.innerHTML = event["location"];
         let eventCohort = document.createElement("li");
         eventCohort.innerHTML = event["cohortId"];
-        eventList.appendChild(eventName, eventdesc, eventDate, eventlocation, eventCohort);
+        let indentedList = document.createElement("ul");
+        indentedList.appendChild(eventdesc);
+        indentedList.appendChild(eventDate);
+        indentedList.appendChild(eventlocation);
+        indentedList.appendChild(eventCohort);
+        eventName.appendChild(indentedList);
+        console.log(eventName, eventdesc, eventDate, eventlocation, eventCohort);
+        eventList.appendChild(eventName);
+        eventName.setAttribute("class", "borderStyle");
+        dButton.innerHTML = "Take event down";
+        eventName.appendChild(dButton);
     });
 }
+
+
+
 
 
 
