88092c5c553c790ad658abc99b8572db2600a940
Amna Abdallah <amnaabdallah@Amnas-MacBook-Air.local>
2023-11-18 13:41:00 -0500

    updating adding and deleting events

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const eventsApiUrl = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-acc-et-web-pt-b/events";
 const eventList = document.getElementById("eventList");
-const dButton = document.createElement("button");
-const addForm = document.getElementById("addEventForm");
+const addForm = document.getElementById("submitButton");
 
 const state = {
     events: []
@@ -14,9 +13,14 @@ async function render() {
 }
 
 async function getEvents() {
-    let fetchApi = await fetch(eventsApiUrl);
-    let resultEvents = await fetchApi.json();
-    state["events"] = resultEvents.data;
+    try{
+        let fetchApi = await fetch(eventsApiUrl);
+        let resultEvents = await fetchApi.json();
+        state["events"] = resultEvents.data;
+    } catch(error) {
+        console.log(error);
+    }
+   
 }
 
 async function renderEvents() {
@@ -32,46 +36,55 @@ async function renderEvents() {
         let eventCohort = document.createElement("li");
         eventCohort.innerHTML = event["cohortId"];
         let indentedList = document.createElement("ul");
+        eventName.setAttribute("class", "borderStyle");
+        let dButton = document.createElement("button");
+        dButton.innerHTML = "Take event down";
+        dButton.addEventListener("click", async ()=> {
+            deleteEvent(event.id);
+        });
         indentedList.appendChild(eventdesc);
         indentedList.appendChild(eventDate);
         indentedList.appendChild(eventlocation);
         indentedList.appendChild(eventCohort);
         eventName.appendChild(indentedList);
-        console.log(eventName, eventdesc, eventDate, eventlocation, eventCohort);
         eventList.appendChild(eventName);
-        eventName.setAttribute("class", "borderStyle");
-        dButton.innerHTML = "Take event down";
         eventName.appendChild(dButton);
     });
 }
 
 
-addForm.addEventListener("submit", async (e)=> {
+addForm.addEventListener("click", async (e)=> {
     e.preventDefault();
-    console.log(e);
-    const name = e.target[0].value;
-    const description = e.target[1].value;
-    const date = e.target[2].value;
-    const location = e.target[3].value;
-    const cohort = e.target[4].value;
-    console.log(name, description, date, location, cohort)
+    const specificDate = new Date(e.target.form[2].value); 
+    const iso8601Date = specificDate.toISOString();
+    fetch("https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-acc-et-web-pt-b/events", {
 
-  const postMethod = await fetch("https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-acc-et-web-pt-b/events", {
-  method: 'POST',
-  body: JSON.stringify({
-    name: name,
-    description: description,
-    date: date,
-    location: location,
-    cohortId: cohort 
-  }),
-  });
-
-  const result = await postMethod.json();
-  console.log(result); 
-
+    method: "POST",
+    body: JSON.stringify({
+        name: e.target.form[0].value,
+        description: e.target.form[1].value,
+        date: iso8601Date,
+        location: e.target.form[3].value,
+        cohortId: e.target.form[4].value
+    }),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+})
+.then(response => response.json())
+.then(json => console.log(json));
+window.location.hash = render();
+console.log(window.location.hash);
 });
 
-
+async function  deleteEvent(id) {
+    try{
+        await fetch(`https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-acc-et-web-pt-b/events/${id}`, { method: 'DELETE' });
+    }catch(error){
+        console.log(error)
+    }
+}
 
 render();
+
+
